1327b09fdd8b9e2576776404cba7fa23affa3770
cop4331 <62314583+cop4331@users.noreply.github.com>
2020-04-21 10:09:10 -0400

    Update models.js

--- models.js
+++ models.js
@@ -14,6 +14,7 @@
  *          email:
  *            type: string
  *            format: email
+ *            description: Must be valid and unique, required for email verification.
  *          password:
  *             type: string
  *        example:
@@ -34,7 +35,41 @@
  *        properties:
  *          username:
  *            type: string
+ *            description: The user who created the post.
  *          text:
+ *            type: string
+ *            description: The content of the post.
+ *        example:
+ *          username: thebestusernameever
+ *          text: I love this app so much! It's so useful!
+ */
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      StepData:
+ *        type: object
+ *        required:
+ *          - userID
+ *          - date
+ *          - numSteps
+ *          - distanceTraveled
+ *          - caloriesBurned
+ *          - dailyGoal
+ *        properties:
+ *          userID:
+ *            type: string
+ *            description: This is the string representing the MongoDB ObjectID of the user.
+ *          date:
+ *            type: string
+ *          numSteps:
+ *            type: string
+ *          distanceTraveled:
+ *            type: string
+ *          caloriesBurned:
+ *            type: string
+ *          dailyGoal:
  *            type: string
  *        example:
  *          username: thebestusernameever
