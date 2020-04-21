f01dcc54b9eaa68f7d9e74b73df90bdcbda3da08
cop4331 <62314583+cop4331@users.noreply.github.com>
2020-04-21 10:46:20 -0400

    Update routes.js

--- routes.js
+++ routes.js
@@ -26,3 +26,25 @@
  *              schema:
  *                $ref: '#/components/schemas/Error'
  */
+
+/**
+ * @swagger
+ * path:
+ *  /login:
+ *    post:
+ *      summary: Log in to a user account. It is not required to send email paramater. If you do, it will be ignored.
+ *      tags: [Users]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      responses:
+ *        "200":
+ *          description: An error message.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/LoginSuccess'
+ */
